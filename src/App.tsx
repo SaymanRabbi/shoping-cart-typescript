b7191a0a07e533713f7c54118fe9bdeec15bc7f8
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-08 13:11:38 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// components
+// import  Drawer  from "@material-ui/core/Drawer";
+import Grid from "@material-ui/core/Grid";
+import LinearProgress from "@material-ui/core/LinearProgress";
+// import  AddShoppingCart  from "@material-ui/icons/AddShoppingCart";
+// import  Badge  from "@material-ui/core/Badge";
+import { useQuery } from "react-query";
+import { AppContainer } from "./app.style";
+import Item from "./item";
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong ...</div>;
+  return (
+    <AppContainer>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} />
+          </Grid>
+        ))}
+      </Grid>
+    </AppContainer>
+  );
+};
 
 export default App;
